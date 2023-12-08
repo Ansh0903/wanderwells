40eed927c24c40c4fc24d65058572756bbeb5932
Ansh Bisht <98746469+Ansh0903@users.noreply.github.com>
2023-12-08 16:16:10 +0000

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .config import config_options
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://langatj:Access@localhost/london'
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
